6befc97b164abda503d339b372b8551d922d9350
Vladcorjuc <47830022+Vladcorjuc@users.noreply.github.com>
2020-04-07 17:31:05 +0300

    Added Building class and Road class

---
+++ Unity/Assets/Scripts/Building.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class Building : MonoBehaviour
+    {
+        private Player owner;
+
+        public Player Owner { get { return owner; } set { this.owner = value; } }
+        public bool isOwned()
+        {
+            if (owner == null)
+                return false;
+            return true;
+        }
+
+    }
+
---
+++ Unity/Assets/Scripts/Road.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road : MonoBehaviour
+{
+    private Player owner;
+
+    public Player Owner
+    {
+        get { return owner;}
+        set { owner = value; }
+    }
+    public bool isOwned()
+    {
+        if (owner == null)
+            return false;
+        return true;
+    }
+    
+
+}
