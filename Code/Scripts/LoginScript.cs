95449f7a31cfb81924eb23270337ab4625a173d3
Tugui Madalina <tuguimadalinaa@gmail.com>
2020-04-08 13:13:26 +0300

    Scripts For End Of Sprint1+RestClient

---
+++ Code/Scripts/LoginScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using FullSerializer;
+using Proyecto26;
+using UnityEngine.SceneManagement;
+
+public class LoginScript : MonoBehaviour
+{
+
+    public InputField Username;
+    public InputField Password;
+    public static int UserAccepted = -2;
+    public static int ButtonPressed = 0;
+    private void CheckCredentials(UserCredentials Checker)
+    {
+        ButtonPressed = 1;
+
+        if (Checker == null)
+        {
+
+            LoginScript.UserAccepted = -1;
+
+        }
+        else
+        {
+            if (Checker.username == Username.text && Checker.userpassword == Password.text)
+            {
+                Debug.Log("Congratulations you succesfully logged on!" + Checker.username);
+                LoginScript.UserAccepted = 1;
+                SceneManager.LoadScene("MenuScene");
+
+            }
+            else if (Checker.username == null)
+            {
+                LoginScript.UserAccepted = -3;
+            }
+            else if (Password.text == "")
+            {
+                LoginScript.UserAccepted = -4;
+            }
+            else
+            {
+
+                LoginScript.UserAccepted = 0;
+
+            }
+
+
+        }
+
+    }
+
+    public static int GetUserStatus()
+    {
+        return LoginScript.UserAccepted;
+    }
+
+    public void LoginRequest()
+    {
+
+        Debug.Log("Username:" + Username.text);
+        DatabaseHandler.GetUserByUsername(Username.text, CheckCredentials);
+    }
+
+}
---
+++ Code/Scripts/UserCredentials.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using FullSerializer;
+using Proyecto26;
+
+[Serializable]
+public class UserCredentials
+{
+    public string username;
+    public string userpassword;
+    public string useremail;
+    public string userId;
+    public string userextension;
+
+    public UserCredentials(string username,string userpassword)
+    {
+        this.username = username;
+        this.userpassword = userpassword;
+        userId = System.Guid.NewGuid().ToString();
+    }
+    public UserCredentials(string username, string userpassword,string useremail)
+    {
+        this.username = username;
+        this.userpassword = userpassword;
+        this.useremail = useremail;
+        userId = System.Guid.NewGuid().ToString();
+    }
+        
+}
