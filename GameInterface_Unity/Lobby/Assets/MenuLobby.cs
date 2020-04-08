8f0d2c963501f873e1091b3e79cd7589aceb69ff
mflorentina-00 <59765478+mflorentina-00@users.noreply.github.com>
2020-04-08 13:19:34 +0300

    Unity

---
+++ GameInterface_Unity/Lobby/Assets/MenuLobby.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MenuLobby : MonoBehaviour
+{
+    public Image CheckImage1;
+    public Image CheckImage2;
+    public Image SelectImage1;
+    public Image SelectImage2;
+    public Image SelectImage3;
+    public Image SelectImage4;
+    public Image SelectImage5;
+    public Image imgExtensii1;
+    public Text textExtensii1;
+    public Image imgExtensii2;
+    public Text textExtensii2;
+    public Image imgExtensii3;
+    public Text textExtensii3;
+    public Image imgExtensii4;
+    public Text textExtensii4;
+    public Image imgExtensii5;
+    public Text textExtensii5;
+
+
+
+    void Start()
+    {
+        CheckImage1 = GameObject.Find("Image1").GetComponent<Image>();
+        CheckImage1.enabled = false;
+
+        CheckImage2 = GameObject.Find("Image2").GetComponent<Image>();
+        CheckImage2.enabled = false;
+
+        SelectImage1 = GameObject.Find("ImageSelectExp1").GetComponent<Image>();
+        SelectImage1.enabled = false;
+
+        SelectImage2 = GameObject.Find("ImageSelectExp2").GetComponent<Image>();
+        SelectImage2.enabled = false;
+
+        SelectImage3 = GameObject.Find("ImageSelectExp3").GetComponent<Image>();
+        SelectImage3.enabled = false;
+
+        SelectImage4 = GameObject.Find("ImageSelectExp4").GetComponent<Image>();
+        SelectImage4.enabled = false;
+
+        SelectImage5 = GameObject.Find("ImageSelectExp5").GetComponent<Image>();
+        SelectImage5.enabled = false;
+
+
+        imgExtensii1 = GameObject.Find("ImageTextExtensie1").GetComponent<Image>();
+        imgExtensii1.enabled = false;
+        textExtensii1 = GameObject.Find("TextExtensie1").GetComponent<Text>();
+        textExtensii1.enabled = false;
+
+        imgExtensii1 = GameObject.Find("ImageTextExtensie2").GetComponent<Image>();
+        imgExtensii1.enabled = false;
+        textExtensii1 = GameObject.Find("TextExtensie2").GetComponent<Text>();
+        textExtensii1.enabled = false;
+
+        imgExtensii1 = GameObject.Find("ImageTextExtensie3").GetComponent<Image>();
+        imgExtensii1.enabled = false;
+        textExtensii1 = GameObject.Find("TextExtensie3").GetComponent<Text>();
+        textExtensii1.enabled = false;
+
+        imgExtensii1 = GameObject.Find("ImageTextExtensie4").GetComponent<Image>();
+        imgExtensii1.enabled = false;
+        textExtensii1 = GameObject.Find("TextExtensie4").GetComponent<Text>();
+        textExtensii1.enabled = false;
+
+        imgExtensii1 = GameObject.Find("ImageTextExtensie5").GetComponent<Image>();
+        imgExtensii1.enabled = false;
+        textExtensii1 = GameObject.Find("TextExtensie5").GetComponent<Text>();
+        textExtensii1.enabled = false;
+
+
+    }
+    public void apasaCheck(Image customImage)
+    {
+
+        CheckImage2.enabled = false;
+        CheckImage1.enabled = false;
+        customImage.enabled = true;
+
+    }
+
+    public void apasaExansion(Image customImage)
+    {
+        SelectImage1.enabled = false;
+        SelectImage2.enabled = false;
+        SelectImage3.enabled = false;
+        SelectImage4.enabled = false;
+        SelectImage5.enabled = false;
+        customImage.enabled = true;
+    }
+
+    
+}
---
+++ GameInterface_Unity/Menu/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using System.Linq;
+
+public class mainMenu : MonoBehaviour
+{
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void goToSettings()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void playOnline()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void playSinglePlayer()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void goBackToMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public AudioMixer audioMixer;
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    Resolution[] resolutions;
+    public Dropdown resolutionDropdown;
+    int currentResolutionIndex = 0;
+    private void Start()
+    {
+        
+        resolutions = Screen.resolutions.Select(resolution => new Resolution { width = resolution.width, height = resolution.height }).Distinct().ToArray();
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        Debug.Log(resolutions.Length);
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+}
