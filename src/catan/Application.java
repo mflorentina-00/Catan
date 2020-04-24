06a51c34a2587a2bcbaf36be366fd6bcf34f6c31
Ojoc Georgiana Diana <georgiana.ojoc@gmail.com>
2020-04-24 19:53:36 +0300

    Added Heroku configuration

--- src/catan/Application.java
+++ src/catan/Application.java
@@ -3,22 +3,34 @@ package catan;
 import catan.game.gameType.Game;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+@Controller
 @SpringBootApplication
 public class Application {
 	public static Map<String, Game> games = new HashMap<>();
 
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
+
+		/*
 		try {
-			ConnectivitySimulation conn = new ConnectivitySimulation();
-			conn.simulation();
+			ConnectivitySimulation connectivity = new ConnectivitySimulation();
+			connectivity.simulation();
 		} catch (InterruptedException | IOException exception) {
 			exception.printStackTrace();
 		}
+		 */
 	}
 }
