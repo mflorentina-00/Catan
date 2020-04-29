adaac4db0db4bd753c52efb4b0509cb3b3e8dbf2
Tugui Madalina <tuguimadalinaa@gmail.com>
2020-04-29 13:54:27 +0300

    added Scripts

--- Code/Scripts/ReceiveBoardScript.cs
+++ Code/Scripts/ReceiveBoardScript.cs
@@ -7,35 +7,45 @@ using UnityEngine;
 using FullSerializer;
 using Proyecto26;
 using UnityEngine.SceneManagement;
+using System.IO;
+using System.Text;
 
-public class ReceiveBoardScript : MonoBehaviour
+[System.Serializable]
+public class ReceiveBoardScript
 {
-    BoardConnectivityJson ReceivedBoard = new BoardConnectivityJson();
+    public BoardConnectivityJson ReceivedBoard = new BoardConnectivityJson();
+    public static string stringBoard;
+
+    public void GetString(string user)
+    {
+        stringBoard = user;
+    }
 
     public void getGameBoard(string ReceivedGameID)
     {
+        string board2 = "";
         GameIDConnectivityJson gameid = new GameIDConnectivityJson();
         gameid.gameid = ReceivedGameID;
+
         RestClient.Post<BoardConnectivityJson>("https://catan-connectivity.herokuapp.com/lobby/startgame", gameid).Then(board =>
         {
             ReceivedBoard.ports = board.ports;
             ReceivedBoard.board = board.board;
-            Debug.Log(JsonUtility.ToJson(ReceivedBoard));
+            string path = "board.json";
+            byte[] bytes = Encoding.ASCII.GetBytes(JsonUtility.ToJson(ReceivedBoard));
+            File.WriteAllBytes(path, bytes);
         }).Catch(err => { Debug.Log(err); });
     }
 
-    public string sendSerializedBoardJson()
-    {
-        return JsonUtility.ToJson(ReceivedBoard);
-    }
 
     public void RequestLobbyidAndGameid()
     {
         UnityConnectivityCommand command = new UnityConnectivityCommand();
+        string board = "";
         command.username = "abcdef";
-        RestClient.Post<LobbyConnectivityJson>("https://catan-connectivity.herokuapp.com/lobby/add", command).Then(ReceivedLobby =>
+       RestClient.Post<LobbyConnectivityJson>("https://catan-connectivity.herokuapp.com/lobby/add", command).Then(ReceivedLobby =>
         {
-            getGameBoard(ReceivedLobby.gameid);
+           getGameBoard(ReceivedLobby.gameid);
         }).Catch(err => { Debug.Log(err); });
     }
 
