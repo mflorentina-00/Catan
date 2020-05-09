b4814ab7406a8e9b76282b92e183ab052012d59b
drgvszi <33577111+drgvszi@users.noreply.github.com>
2020-05-09 09:28:34 +0300

    token

---
+++ UnityProject/Assets/Scripts/Token.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Token 
+{
+    private static string ApiToken;
+    public static void SetToken(string Token)
+    {
+        ApiToken = Token;
+    }
+    public static string GetToken()
+    {
+        return ApiToken;
+    }
+}
